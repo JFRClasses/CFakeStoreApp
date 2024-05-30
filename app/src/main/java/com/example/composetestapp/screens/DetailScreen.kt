1ffea11c8c7fd97b30aa7c794edfef109ba62c66
juanfrausto <juanfr97@hotmail.com>
2024-05-30 13:11:57 -0600

    Simple retrofit implemented

--- app/src/main/java/com/example/composetestapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/composetestapp/screens/DetailScreen.kt
@@ -1,5 +1,7 @@
 package com.example.composetestapp.screens
 
+import android.util.Log
+import android.widget.ProgressBar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,9 +16,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,88 +39,138 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.composetestapp.R
 import com.example.composetestapp.models.Product
+import com.example.composetestapp.models.Rating
+import com.example.composetestapp.services.ProductService
 import com.example.composetestapp.ui.theme.Purple40
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
 fun DetailScreen(productId:Int){
-    val product = Product.productList.first { it.id == productId }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp)
-    ) {
-        Text(
-            text = product.title,
+    var product by remember {
+        mutableStateOf(Product(
+            id = 0,
+            title = "",
+            description = "",
+            price = 0.0,
+            category = "",
+            rating = Rating(count = 4, rate = 2.1),
+            image = ""
+        ))
+    }
+    var isLoading by remember { mutableStateOf(false)}
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(key1 = true) {
+        scope.launch {
+            Log.d("HomeScreen", "LaunchedEffect")
+            val BASE_URL = "https://fakestoreapi.com/"
+            val builder = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ProductService::class.java)
+            isLoading  = true
+            val response = builder.getProductById(productId)
+            Log.i("HomeScreen", response.toString())
+            withContext(Dispatchers.IO) {
+                product = response
+                isLoading = false
+                Log.i("HomeScreen", product.toString())
+            }
+        }
+    }
+    if(isLoading){
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+
+        ){
+            CircularProgressIndicator()
+        }
+    }
+    else {
+        Column(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(bottom = 10.dp),
-            style = TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-        )
-        AsyncImage(
-            model = product.image,
-            contentDescription = null,
-            placeholder = painterResource(id = R.drawable.ic_launcher_background),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )
-        Text(
-            product.description,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth()
-                .padding(top = 10.dp),
-            style = TextStyle(
-                color = Color.DarkGray,
-                fontSize = 16.sp,
-                letterSpacing = 2.sp
-            )
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp)
+                .fillMaxSize()
+                .padding(10.dp)
         ) {
-            Icon(
-                imageVector = Icons.Default.Star,
-                contentDescription = "Rating",
-                tint = Color.Yellow,
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
             Text(
-                text = product.rating.rate.toString(),
-                modifier = Modifier.align(Alignment.CenterVertically)
+                text = product.title,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 10.dp),
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
             )
-            Spacer(modifier = Modifier.weight(1f))
-            Box(
+            AsyncImage(
+                model = product.image,
+                contentDescription = null,
+                placeholder = painterResource(id = R.drawable.ic_launcher_background),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.CenterVertically)
-                    .background(
-                        color = Purple40,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .weight(1f)
-                    .padding(5.dp)
-            ) {
-                Text(
-                    text = product.category,
-                    color = Color.White,
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                    ),
-                    modifier = Modifier.align(Alignment.Center)
+                    .height(200.dp)
+            )
+            Text(
+                product.description,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 16.sp,
+                    letterSpacing = 2.sp
                 )
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Rating",
+                    tint = Color.Yellow,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+                Text(
+                    text = product.rating.rate.toString(),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.CenterVertically)
+                        .background(
+                            color = Purple40,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .weight(1f)
+                        .padding(5.dp)
+                ) {
+                    Text(
+                        text = product.category,
+                        color = Color.White,
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                        ),
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+
             }
+            Spacer(modifier = Modifier.weight(1f))
+            Text(product.computedPrice)
 
         }
-        Spacer(modifier = Modifier.weight(1f))
-        Text(product.computedPrice)
-
     }
+
 
 }
 
--- app/src/main/java/com/example/composetestapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/composetestapp/screens/HomeScreen.kt
@@ -1,11 +1,13 @@
 package com.example.composetestapp.screens
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,9 +19,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,104 +43,146 @@ import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.example.composetestapp.R
 import com.example.composetestapp.models.Product
+import com.example.composetestapp.services.ProductService
 import com.example.composetestapp.ui.theme.Purple40
 import com.example.composetestapp.ui.theme.PurpleGrey80
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
 fun HomeScreen(navController:NavController){
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier.padding(10.dp)
-    ){
-        items(Product.productList){
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .clickable {
-                       navController.navigate("detail/${it.id}")
-                    }
-                    .padding(5.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = PurpleGrey80
-                ),
-                elevation = CardDefaults.cardElevation(10.dp)
-            ) {
-                Column(
-                    modifier = Modifier.padding(5.dp)
-                ) {
-                    AsyncImage(
-                        model = it.image,
-                        contentDescription = null,
-                        placeholder = painterResource(id = R.drawable.ic_launcher_background),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(3f)
-                    )
-                    Text(
-                        text = it.computedTitle,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.5f),
-                        style = TextStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 16.sp
-                        )
-                    )
-                    Text(
-                        text = it.category,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.5f),
-                        style = TextStyle(
-                            fontWeight = FontWeight.W300,
-                            fontSize = 12.sp,
-                            color = Color.DarkGray
-                        )
-                    )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Star,
-                            contentDescription = "Rating",
-                            tint = Color.Yellow,
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                        Text(
-                            text = it.rating.rate.toString(),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.CenterVertically)
-                                .background(
-                                    color = Purple40,
-                                    shape = RoundedCornerShape(8.dp)
-                                )
-                                .weight(1f)
-                                .padding(5.dp)
-                        ) {
-                            Text(
-                                text = it.computedPrice,
-                                color = Color.White,
-                                style = TextStyle(
-                                    fontSize = 12.sp
-                                )
-                            )
-                        }
-
-                    }
-                }
+    var products by remember {
+        mutableStateOf(listOf<Product>())
+    }
+    var isLoading by remember {
+        mutableStateOf(false)
+    }
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(key1 = true) {
+        scope.launch {
+            Log.d("HomeScreen", "LaunchedEffect")
+            val BASE_URL = "https://fakestoreapi.com/"
+            val builder = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ProductService::class.java)
+            isLoading = true
+            val response = builder.getProducts()
+            Log.i("HomeScreen", response.toString())
+            withContext(Dispatchers.IO) {
+                products = response
+                isLoading = false
+                Log.i("HomeScreen", products.toString())
             }
-
-
         }
     }
+    if(isLoading){
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+            CircularProgressIndicator()
+        }
+
+    }
+    else {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.padding(10.dp)
+        ){
+            items(products){
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clickable {
+                            navController.navigate("detail/${it.id}")
+                        }
+                        .padding(5.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = PurpleGrey80
+                    ),
+                    elevation = CardDefaults.cardElevation(10.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+                        AsyncImage(
+                            model = it.image,
+                            contentDescription = null,
+                            placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(3f)
+                        )
+                        Text(
+                            text = it.computedTitle,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(0.5f),
+                            style = TextStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp
+                            )
+                        )
+                        Text(
+                            text = it.category,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(0.5f),
+                            style = TextStyle(
+                                fontWeight = FontWeight.W300,
+                                fontSize = 12.sp,
+                                color = Color.DarkGray
+                            )
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Star,
+                                contentDescription = "Rating",
+                                tint = Color.Yellow,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                            Text(
+                                text = it.rating.rate.toString(),
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .align(Alignment.CenterVertically)
+                                    .background(
+                                        color = Purple40,
+                                        shape = RoundedCornerShape(8.dp)
+                                    )
+                                    .weight(1f)
+                                    .padding(5.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = it.computedPrice,
+                                    color = Color.White,
+                                    style = TextStyle(
+                                        fontSize = 12.sp
+                                    )
+                                )
+                            }
+
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+
 }
 
 @Preview(
